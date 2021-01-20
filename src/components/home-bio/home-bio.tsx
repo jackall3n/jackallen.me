988454b18a57becee1edfece7377dc4f240d2808
Jack Allen <jackallensemail+git@gmail.com>
2021-01-20 12:10:52 +0000

    comma

--- src/components/home-bio/home-bio.tsx
+++ src/components/home-bio/home-bio.tsx
@@ -20,23 +20,6 @@ function HomeBio() {
 
   const avatar = data?.avatar?.childImageSharp?.fixed
 
-  const typed = useMemo(() => {
-    const languages = ["Javascript", "C#", "Swift", "Java", "Python", "CSS", "SASS", "Objective-C", "React Native", "SQL", "Angular"].map(h => `writes ${h}.`)
-    const loves = ["React", "Typescript", "Kubernetes", "Docker", "GraphQL", "JetBrains"].map(h => `loves ${h}!`)
-    const uses = ["PostgreSQL", "Terraform", "Node"].map(h => `uses ${h}.`)
-    const builds = ["with Raspberry Pis", "Mobile Apps"].map(h => `builds ${h}.`)
-    const avoids = ["PHP"].map(h => `avoids ${h}.`)
-    const prefers = ["Spaces ⌨️"].map(h => `prefers ${h}.`)
-    const eats = ["anything 🥑"].map(h => `eats ${h}.`)
-
-    const typed = [...languages, ...loves, ...avoids, ...prefers, ...eats, ...uses, ...builds]
-
-    typed.sort(() => .5 - Math.random())
-
-    return typed
-  }, [])
-
-
   return (
     <div className="home-bio">
       <Image
@@ -48,7 +31,7 @@ function HomeBio() {
         }}
       />
       <div className="home-bio-hi">Hi, I'm Jack 👋</div>
-      <div className="home-bio-summary">Building <span className="highlight">digital<br /> products</span>, brands
+      <div className="home-bio-summary">Building <span className="highlight">digital<br /> products</span>, brands,
         and<br /> experience.
       </div>
       <div className="home-bio-description">
